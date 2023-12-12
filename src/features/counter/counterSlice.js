316a72aae2664252fff84e4522c4146f426953ed
NazmulAlamShawon <shawonkst15@gmail.com>
2023-12-12 19:44:29 +0600

    add new reducer

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -5,5 +5,17 @@ const initialState = {
     count: 0
 }
 
-export const  counterSlce = createSlice({})
+export const  counterSlce = createSlice({
+    name : 'counter',
+    initialState,
+    reducers:{
+        increment: (state) => {
+            state.count += 1;
+        },
+        decrement: (state) => {
+            state.count -= 1;
+        },
+    }
+
+})
 
